dee096962da88d77f0d5d7af64fc933ac6f25877
GhostPunishR <68479390+GhostPunishR@users.noreply.github.com>
2026-07-14 11:03:24 +0200

    feat: export anti-nuke enforcement API

--- src/adapters/discordjs/index.ts
+++ src/adapters/discordjs/index.ts
@@ -1,3 +1,12 @@
 export { DiscordJsAntiNuke } from "./DiscordJsAntiNuke.js";
+export { enforceAntiNukeIncident } from "./enforcement.js";
 export { toGuildMutationEvent } from "./toGuildMutationEvent.js";
-export type { DiscordJsAntiNukeAdapter, DiscordJsAntiNukeOptions } from "./types.js";
+export type {
+  DiscordJsAntiNukeAdapter,
+  DiscordJsAntiNukeEnforcementAction,
+  DiscordJsAntiNukeEnforcementErrorContext,
+  DiscordJsAntiNukeEnforcementOptions,
+  DiscordJsAntiNukeEnforcementResult,
+  DiscordJsAntiNukeEnforcementStatus,
+  DiscordJsAntiNukeOptions,
+} from "./types.js";
